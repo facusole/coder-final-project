a411c0571afab5109ead638fa83865615ad81145
Facundo <facundojsole@gmail.com>
2023-02-16 22:24:25 -0300

    add better html semantic and new css variables

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,12 @@ function App() {
 
   return (
     <div >
+      <header>
         <Navbar />
+      </header>
+      <main>
         <ItemListContainer greeting={'Tutor'}/>
+      </main>
     </div>
   )
 }

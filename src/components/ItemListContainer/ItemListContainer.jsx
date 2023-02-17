3e8f06f0dd689c88e48629aea62637a87f457c00
Facundo <facundojsole@gmail.com>
2023-02-16 22:24:46 -0300

    add better semantic html and add bg color in css

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,8 +2,8 @@ import './ItemListContainer.css'
 
 export default function ItemListContainer({greeting}) {
     return (
-        <div className='itemList__container mg__inline'>
+        <section className='itemList__container mg__inline'>
             <h1 className=".greetings">Hola, {greeting}</h1>
-        </div>
+        </section>
     )
 }

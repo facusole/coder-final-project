a1792462b6d244fe6f2c56b7620dae2f6c5f80b3
Facundo <facundojsole@gmail.com>
2023-02-22 20:30:30 -0300

    PreEntrega1 - Solé

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,10 @@
 import './ItemListContainer.css'
 
 export default function ItemListContainer({greeting}) {
+
     return (
         <section className='itemList__container mg__inline'>
-            <h1 className=".greetings">Hola, {greeting}</h1>
+            <h1 className=".greetings">Hola, {greeting}</h1>            
         </section>
     )
 }

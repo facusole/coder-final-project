42bc2b9652eaa424e970d1f452dae48598dd72d7
Facundo <facundojsole@gmail.com>
2023-03-07 17:43:07 -0300

    add item rendering

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,58 @@
+import { useState, useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { pedirDatos } from '../../utils/pedirDatos'
+
+import Item from '../Item/Item'
 import './ItemListContainer.css'
 
-export default function ItemListContainer({greeting}) {
+
+
+export default function ItemListContainer() {
+
+    let [productos, setProductos] = useState([])
+    let [isLoading, setIsLoading] = useState(true)
+
+    const {categoryId} = useParams()
+    console.log(categoryId)
+
+
+    useEffect(() => {
+
+        setIsLoading(true)
+
+        pedirDatos()
+            .then((response) => {
+                !categoryId ? 
+                setProductos(response) 
+                :
+                setProductos(response.filter((prod) => prod.category === categoryId))
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+            .finally(() => {
+                setIsLoading(false)
+            })
+
+    }, [categoryId])
+
+
+
 
     return (
-        <section className='itemList__container mg__inline'>
-            <h1 className=".greetings">Hola, {greeting}</h1>            
-        </section>
+        <>
+
+            {isLoading == true ?
+                <h2>Cargando...</h2>
+                :
+                <div style={{ display: 'flex', flexWrap: 'wrap', gap: 64, marginTop: 32 }}>
+                    {productos.map((item) =>
+                        <Item key={item.id} item={item}/>
+                    )}
+                </div>
+            }
+
+        </>
+
     )
 }

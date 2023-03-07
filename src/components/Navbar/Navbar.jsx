696756a38fa13013c0e41ab81379ca8784f460e1
Facundo <facundojsole@gmail.com>
2023-03-07 17:44:03 -0300

    add routing in the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,12 +2,13 @@ import CartWidget from './CartWidget/CartWidget';
 import BrandLogo from '../../assets/pluma-logo.webp'
 
 import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
         <nav className='navbar__container mg__inline'>
             <div className="brand__container">
-                <a href="#"><img src={BrandLogo} aria-label='Pluma de Papel' className='brand__logo' alt="Logo de la marca Pluma de papel" /></a>
+                <Link to='/'><img src={BrandLogo} aria-label='Pluma de Papel' className='brand__logo' alt="Logo de la marca Pluma de papel" /></Link>
                 {/* <h2 className="brand__name">
                     Pluma de Papel
                 </h2> */}
@@ -25,9 +26,9 @@ export default function Navbar() {
             </div>
             <div>
                 <ul className="menu__container">
-                    <li><a href="#" className="categories">Juegos de mesa</a></li>
-                    <li><a href="#" className="categories">Útiles escolares</a></li>
-                    <li><a href="#" className="categories">Cuadernos</a></li>
+                    <li><Link to="/category/juegos-de-mesa" className="categories">Juegos de mesa</Link></li>
+                    <li><Link to="/category/utiles-escolares" className="categories">Útiles escolares</Link></li>
+                    <li><Link to="/category/cuadernos" className="categories">Cuadernos</Link></li>
                     <li className="shopping__cart">
                         <CartWidget />
                         <div className="hardcoded__number"><div className="bg__color">4</div></div>

80342d7ecb3552f8323a728beafde865425a7c33
Facundo <facundojsole@gmail.com>
2023-03-23 15:59:53 -0300

    add context and context functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
+import { useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
-import ItemDetailContainer from "./components/ItemListContainer/ItemDetailContainer/ItemDetailContainer";
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
+import Cart from "./components/Cart/Cart";
+import { CartContext } from "./components/context/CartContext";
 
 import './App.css';
 
 function App() {
 
+  const [cart, setCart] = useState([])
+  console.log(cart)
+
+  const handleAddItem = (item) => {
+    setCart([...cart, item])
+    console.log(cart)
+  }
+
+  const isInCart = (id) => cart.some(prod => prod.id === id)
+
+  const totalQuantity = () => {
+    cart.reduce((acc, prod) => acc + prod.quantity, 0)
+  }
+
   return (
-    <div >
+    <CartContext.Provider value={{
+      cart,
+      handleAddItem,
+      isInCart,
+      totalQuantity
+    }}>
+
       <header>
         <Navbar />
       </header>
@@ -17,10 +40,12 @@ function App() {
           <Route path="/" element={<ItemListContainer />}/>
           <Route path="/category/:categoryId" element={<ItemListContainer />}/>
           <Route path="/item/:itemId" element={<ItemDetailContainer/>} />
+          <Route path="/cart" element={<Cart/>} />
           <Route path="*" element={<Navigate to='/' />}/>
         </Routes>
       </main>
-    </div>
+
+    </CartContext.Provider>
   )
 }
 
